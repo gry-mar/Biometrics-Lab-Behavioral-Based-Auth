1964c8569fc9f6062545cff23fad9efad1cb6567
martynagrygiel <grygielmartyna439@gmail.com>
2024-05-12 03:51:24 +0200

    streamlit ui wip

--- src/app.py
+++ src/app.py
@@ -5,28 +5,62 @@ import os
 
 st.title('🎶AA🎶 - the best Audio Auth')
 
-# Sound input section
-st.subheader("Upload or Record Audio")
-audio_file = st.file_uploader("Choose an audio file or record one:", type=['wav', 'mp3', 'ogg'])
-# audio_data = st_audio_recorder(rec='Record new audio', play='Play', download='Download', save='Save', upload='Upload')
 
-wav_audio_data = st_audiorec()
+st.subheader("Record file or choose from disk")
 
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
+if "audio_option" not in st.session_state:
+    st.session_state.audio_option = "Load"
+    
+st.radio(
+        label="Choose",
+        options=["Record", "Load"],
+        key="audio_option",
+    )
 
-if audio_file is not None:
-    # Save the file to the server temporarily
-    with open(os.path.join("tempDir",audio_file.name),"wb") as f:
-        f.write(audio_file.getbuffer())
+if st.session_state.audio_option == "Record":
+    audio_file = st_audiorec()
+else: 
+    audio_file = st.file_uploader("Choose an audio file", type=['wav', 'mp3'])
+st.subheader("Select auth mode")
+# if wav_audio_data is not None:
+#         st.audio(wav_audio_data, format='audio/wav')
 
-    if st.button('Confirm Audio'):
-        st.success('Audio confirmed!')
+tab1, tab2= st.tabs(["Check identity", "Authorize as an user"])
 
-    st.audio(audio_file)
+with tab1:
+    
+    if audio_file is not None:
+        # Save the file to the server temporarily
+        with open(os.path.join("tempDir",audio_file.name),"wb") as f:
+            f.write(audio_file.getbuffer())
 
-user_input = st.text_input("Enter some text")
+        if st.button('Confirm Audio'):
+            st.success('Audio confirmed!')
+
+        # st.audio(audio_file)
+
+    if st.button('Check'):
+        pass
+
+with tab2:
+
+    # wav_audio_data = st_audiorec()
+
+    # if wav_audio_data is not None:
+    #     st.audio(wav_audio_data, format='audio/wav')
+
+    if audio_file is not None:
+        # Save the file to the server temporarily
+        with open(os.path.join("tempDir",audio_file.name),"wb") as f:
+            f.write(audio_file.getbuffer())
+
+        if st.button('Confirm Audio'):
+            st.success('Audio confirmed!')
+
+        st.audio(audio_file)
+
+    user_input = st.text_input("Enter some text")
 
 
-if st.button('Click me!'):
-    st.write(f"You entered: {user_input}")
+    if st.button('Click me!'):
+        st.write(f"You entered: {user_input}")
